89edc8aba54d3df85f4579f3d215627f45bcaa0b
Michael J. Ryan <tracker1@gmail.com>
2017-01-30 17:36:33 -0700

    Update index.js
    make results chainable stubs by default...  NOTE: those entries not returning self, should be updated.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import sinon from 'sinon'
 
 // Returns a new mock request for use in testing.
 export const mockReq = (options = {}) => {
-  return Object.assign({
-    accepts: sinon.spy(),
-    acceptsCharsets: sinon.spy(),
-    acceptsEncodings: sinon.spy(),
-    acceptsLanguages: sinon.spy(),
+  const ret = {};
+  return Object.assign(ret, {
+    accepts: sinon.stub().returns(ret),
+    acceptsCharsets: sinon.stub().returns(ret),
+    acceptsEncodings: sinon.stub().returns(ret),
+    acceptsLanguages: sinon.stub().returns(ret),
     body: {},
-    flash: sinon.spy(),
-    get: sinon.spy(),
-    is: sinon.spy(),
+    flash: sinon.stub().returns(ret),
+    get: sinon.stub().returns(ret),
+    is: sinon.stub().returns(ret),
     params: {},
     query: {},
     session: {},
@@ -19,29 +20,30 @@ export const mockReq = (options = {}) => {
 
 // Returns a new mock response for use in testing.
 export const mockRes = (options = {}) => {
-  return Object.assign({
-    append: sinon.spy(),
-    attachement: sinon.spy(),
-    clearCookie: sinon.spy(),
-    cookie: sinon.spy(),
-    download: sinon.spy(),
-    end: sinon.spy(),
+  const ret = {};
+  return Object.assign(ret, {
+    append: sinon.stub().returns(ret),
+    attachement: sinon.stub().returns(ret),
+    clearCookie: sinon.stub().returns(ret),
+    cookie: sinon.stub().returns(ret),
+    download: sinon.stub().returns(ret),
+    end: sinon.stub().returns(ret),
     format: {},
-    get: sinon.spy(),
-    headersSent: sinon.spy(),
-    json: sinon.spy(),
-    jsonp: sinon.spy(),
-    links: sinon.spy(),
+    get: sinon.stub().returns(ret),
+    headersSent: sinon.stub().returns(ret),
+    json: sinon.stub().returns(ret),
+    jsonp: sinon.stub().returns(ret),
+    links: sinon.stub().returns(ret),
     locals: {},
-    location: sinon.spy(),
-    redirect: sinon.spy(),
-    render: sinon.spy(),
-    send: sinon.spy(),
-    sendFile: sinon.spy(),
-    sendStatus: sinon.spy(),
-    set: sinon.spy(),
-    status: sinon.spy(),
-    type: sinon.spy(),
-    vary: sinon.spy(),
+    location: sinon.stub().returns(ret),
+    redirect: sinon.stub().returns(ret),
+    render: sinon.stub().returns(ret),
+    send: sinon.stub().returns(ret),
+    sendFile: sinon.stub().returns(ret),
+    sendStatus: sinon.stub().returns(ret),
+    set: sinon.stub().returns(ret),
+    status: sinon.stub().returns(ret),
+    type: sinon.stub().returns(ret),
+    vary: sinon.stub().returns(ret),
   }, options)
 }
